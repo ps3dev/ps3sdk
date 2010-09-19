141b73420d55bfac8e3513a68f98e0313d866939
John_K <john@kelley.ca>
2010-09-19 13:14:41 -0700

    Add byteswap functions into endian.h since not all platforms have byteswap.h

--- tools/endian.h
+++ tools/endian.h
@@ -6,7 +6,17 @@
 #ifndef __ENDIAN_H__
 #define __ENDIAN_H__
 
-#include <byteswap.h>
+static inline unsigned short bswap_16(unsigned short x) {
+  return (x>>8) | (x<<8);
+}
+
+static inline unsigned int bswap_32(unsigned int x) {
+  return (bswap_16(x&0xffff)<<16) | (bswap_16(x>>16));
+}
+
+static inline unsigned long long bswap_64(unsigned long long x) {
+  return (((unsigned long long)bswap_32(x&0xffffffffull))<<32) | (bswap_32(x>>32));
+}
 
 // #define __BIG_ENDIAN__
 
